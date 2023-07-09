54647ec96eeb6ae117d66ad5987cc2289c8383e0
Daniil Surnin <cyrnin@gmail.com>
2023-07-09 10:47:30 +0300

    Add waits

--- test_items.py
+++ test_items.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 def test_add_to_cart_button_is_displayed(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
+    WebDriverWait(browser, 5).until(ec.visibility_of_element_located((By.XPATH, '//*[@id="add_to_basket_form"]/button')))
     assert browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button'), 'button is not found'
