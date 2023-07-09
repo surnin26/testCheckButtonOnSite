f04c9f59035ba5a3fac0d5c6f6b9970bfe7d9b4a
Daniil Surnin <cyrnin@gmail.com>
2023-07-09 10:38:49 +0300

    Correct test name

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-def test_add_to_cart_button(browser):
+def test_add_to_cart_button_is_displayed(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     assert browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button'), 'button is not found'
